b3214bd2742b5411d42f15d8bb2c7aedd406b748
Codename Anroid <codenamegit@gmail.com>
2012-10-04 16:45:16 +0800

    Prop Modder

--- src/com/android/settings/util/CMDProcessor.java
+++ src/com/android/settings/util/CMDProcessor.java
@@ -1,69 +1,184 @@
 package com.android.settings.util;
 
-import android.util.Log;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
-// convenience import for quick referencing of this method
+import android.util.Log;
 
-public final class CMDProcessor {
-    private static final String TAG = "CMDProcessor";
+public class CMDProcessor {
 
-    private CMDProcessor() {
-        // Cannot instantiate this class
-        throw new AssertionError();
+    private static final String LD_LIBRARY_PATH = System.getenv("LD_LIBRARY_PATH");
+    private static final String TAG = "CMD Processor";
+    private Boolean can_su;
+    public SH sh;
+    public SH su;
+
+    public CMDProcessor() {
+        sh = new SH("sh");
+        su = new SH("su");
     }
 
-    /* Run a system command with full redirection */
-    public static ChildProcess startSysCmd(String[] cmdarray, String childStdin) {
-        return new ChildProcess(cmdarray, childStdin);
+    public SH suOrSH() {
+        return canSU() ? su : sh;
     }
 
-    public static CommandResult runSysCmd(String[] cmdarray, String childStdin) {
-        ChildProcess proc = startSysCmd(cmdarray, childStdin);
-        proc.waitFinished();
-        return proc.getResult();
+    public boolean canSU() {
+        return canSU(false);
     }
 
-    public static ChildProcess startShellCommand(String cmd) {
-        String[] cmdarray = new String[3];
-        cmdarray[0] = "sh";
-        cmdarray[1] = "-c";
-        cmdarray[2] = cmd;
-        return startSysCmd(cmdarray, null);
+    public class CommandResult {
+        public final String stdout;
+        public final String stderr;
+        public final Integer exit_value;
+
+        CommandResult(final Integer exit_value_in) {
+            this(exit_value_in, null, null);
+        }
+
+        CommandResult(final Integer exit_value_in, final String stdout_in,
+                final String stderr_in) {
+            exit_value = exit_value_in;
+            stdout = stdout_in;
+            stderr = stderr_in;
+        }
+
+        public boolean success() {
+            return exit_value != null && exit_value == 0;
+        }
     }
 
-    public static CommandResult runShellCommand(String cmd) {
-        ChildProcess proc = startShellCommand(cmd);
-        proc.waitFinished();
-        return proc.getResult();
+    public class SH {
+        private String SHELL = "sh";
+
+        public SH(final String SHELL_in) {
+            SHELL = SHELL_in;
+        }
+
+        private String getStreamLines(final InputStream is) {
+            String out = null;
+            StringBuffer buffer = null;
+            final DataInputStream dis = new DataInputStream(is);
+
+            try {
+                if (dis.available() > 0) {
+                    buffer = new StringBuffer(dis.readLine());
+                    while (dis.available() > 0) {
+                        buffer.append("\n").append(dis.readLine());
+                    }
+                }
+                dis.close();
+            } catch (final Exception ex) {
+                Log.e(TAG, ex.getMessage());
+            }
+            if (buffer != null) {
+                out = buffer.toString();
+            }
+            return out;
+        }
+
+        public Process run(final String cmd) {
+            Process process = null;
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                process = runtime.exec(SHELL);
+                final DataOutputStream toProcess = new DataOutputStream(
+                        process.getOutputStream());
+                // On some versions of Android (ICS) LD_LIBRARY_PATH is unset when using su
+                // We need to pass LD_LIBRARY_PATH over su for some commands to work correctly.
+                String setenv = "";
+                if ("su".equals(SHELL)) {
+                    setenv = "LD_LIBRARY_PATH=" + LD_LIBRARY_PATH + " ";
+                }
+                toProcess.writeBytes(setenv + "exec " + cmd + "\n");
+                toProcess.flush();
+            } catch (final Exception e) {
+                Log.e(TAG, "Exception while trying to run: '" + cmd + "' "
+                        + e.getMessage());
+                process = null;
+            }
+            return process;
+        }
+
+        public CommandResult runWaitFor(final String s) {
+            final Process process = run(s);
+            Integer exit_value = null;
+            String stdout = null;
+            String stderr = null;
+            if (process != null) {
+                try {
+                    exit_value = process.waitFor();
+                    stdout = getStreamLines(process.getInputStream());
+                    stderr = getStreamLines(process.getErrorStream());
+                } catch (final InterruptedException e) {
+                    Log.e(TAG, "runWaitFor " + e.toString());
+                } catch (final NullPointerException e) {
+                    Log.e(TAG, "runWaitFor " + e.toString());
+                }
+            }
+            return new CommandResult(exit_value, stdout, stderr);
+        }
+
+        public Process run(final String[] cmds) {
+            Process process = null;
+            try {
+                process = Runtime.getRuntime().exec(SHELL);
+                final DataOutputStream toProcess = new DataOutputStream(
+                        process.getOutputStream());
+                // On some versions of Android (ICS) LD_LIBRARY_PATH is unset when using su
+                // We need to pass LD_LIBRARY_PATH over su for some commands to work correctly.
+                String setenv = "";
+                if (SHELL.equals("su")) {
+                    setenv = "LD_LIBRARY_PATH=" + LD_LIBRARY_PATH + " ";
+                }
+                for (String cmd : cmds) {
+                    toProcess.writeBytes(setenv + cmd + "\n");
+                }
+                toProcess.writeBytes("exit\n");
+                toProcess.flush();
+            } catch (final Exception e) {
+                Log.e(TAG, "Exception while trying to run cmds"
+                        + e.getMessage());
+                process = null;
+            }
+            return process;
+        }
+
+        public CommandResult runWaitFor(final String cmds[]) {
+            final Process process = run(cmds);
+            Integer exit_value = null;
+            String stdout = null;
+            String stderr = null;
+            if (process != null) {
+                try {
+                    exit_value = process.waitFor();
+                    stdout = getStreamLines(process.getInputStream());
+                    stderr = getStreamLines(process.getErrorStream());
+                } catch (final InterruptedException e) {
+                    Log.e(TAG, "runWaitFor " + e.toString());
+                } catch (final NullPointerException e) {
+                    Log.e(TAG, "runWaitFor " + e.toString());
+                }
+            }
+            return new CommandResult(exit_value, stdout, stderr);
+        }
     }
 
-    public static ChildProcess startSuCommand(String cmd) {
-        String[] cmdarray = new String[3];
-        cmdarray[0] = "su";
-        cmdarray[1] = "-c";
-        cmdarray[2] = cmd;
-        return startSysCmd(cmdarray, null);
-    }
+    public boolean canSU(final boolean force_check) {
+        if (can_su == null || force_check) {
+            final CommandResult r = su.runWaitFor("id");
+            final StringBuilder out = new StringBuilder();
 
-    public static CommandResult runSuCommand(String cmd) {
-        ChildProcess proc = startSuCommand(cmd);
-        proc.waitFinished();
-        return proc.getResult();
-    }
+            if (r.stdout != null) {
+                out.append(r.stdout).append(" ; ");
+            }
+            if (r.stderr != null) {
+                out.append(r.stderr);
+            }
 
-    public static boolean canSU() {
-        CommandResult r = runShellCommand("id");
-        StringBuilder out = new StringBuilder(0);
-        out.append(r.getStdout());
-        out.append(" ; ");
-        out.append(r.getStderr());
-        Log.d(TAG, "canSU() su[" + r.getExitValue() + "]: " + out);
-        return r.success();
+            Log.d(TAG, "canSU() su[" + r.exit_value + "]: " + out);
+            can_su = r.success();
+        }
+        return can_su;
     }
 }
--- src/com/android/settings/util/Helpers.java
+++ src/com/android/settings/util/Helpers.java
@@ -1,85 +1,50 @@
-
 package com.android.settings.util;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.SystemProperties;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Date;
-
-// don't show unavoidable warnings
-@SuppressWarnings({
-        "UnusedDeclaration",
-        "MethodWithMultipleReturnPoints",
-        "ReturnOfNull",
-        "NestedAssignment",
-        "DynamicRegexReplaceableByCompiledPattern",
-        "BreakStatement"})
 public class Helpers {
-    // avoids hardcoding the tag
-    private static final String TAG = Thread.currentThread().getStackTrace()[1].getClassName();
 
-    public Helpers() {
-        // dummy constructor
-    }
+    private static final String TAG = "Helpers";
 
     /**
      * Checks device for SuperUser permission
      *
      * @return If SU was granted or denied
      */
-    @SuppressWarnings("MethodWithMultipleReturnPoints")
     public static boolean checkSu() {
         if (!new File("/system/bin/su").exists()
                 && !new File("/system/xbin/su").exists()) {
-            Log.e(TAG, "su binary does not exist!!!");
+            Log.e(TAG, "su does not exist!!!");
             return false; // tell caller to bail...
         }
+
         try {
-            if (CMDProcessor.runSuCommand("ls /data/app-private").success()) {
+            if ((new CMDProcessor().su
+                    .runWaitFor("ls /data/app-private")).success()) {
                 Log.i(TAG, " SU exists and we have permission");
                 return true;
             } else {
-                Log.i(TAG, " SU exists but we don't have permission");
+                Log.i(TAG, " SU exists but we dont have permission");
                 return false;
             }
-        } catch (NullPointerException e) {
-            Log.e(TAG, "NullPointer throw while looking for su binary", e);
+        } catch (final NullPointerException e) {
+            Log.e(TAG, e.getLocalizedMessage().toString());
             return false;
         }
-    }
-
-    /**
-     * Checks device for network connectivity
-     *
-     * @return If the device has data connectivity
-    */
-    public static boolean isNetworkAvailable(Context context) {
-        boolean state = false;
-        if (context != null) {
-            ConnectivityManager cm =
-                    (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo netInfo = cm.getActiveNetworkInfo();
-            if (netInfo != null && netInfo.isConnected()) {
-                Log.i(TAG, "The device currently has data connectivity");
-                state = true;
-            } else {
-                Log.i(TAG, "The device does not currently have data connectivity");
-                state = false;
-            }
-        }
-        return state;
     }
 
     /**
@@ -93,227 +58,184 @@ public class Helpers {
             Log.e(TAG, "Busybox not in xbin or bin!");
             return false;
         }
+
         try {
-            if (!CMDProcessor.runSuCommand("busybox mount").success()) {
-                Log.e(TAG, "Busybox is there but it is borked! ");
+            if (!new CMDProcessor().su.runWaitFor("busybox mount").success()) {
+                Log.e(TAG, " Busybox is there but it is borked! ");
                 return false;
             }
-        } catch (NullPointerException e) {
-            Log.e(TAG, "NullpointerException thrown while testing busybox", e);
+        } catch (final NullPointerException e) {
+            Log.e(TAG, e.getLocalizedMessage().toString());
             return false;
         }
         return true;
     }
 
-    public static String[] getMounts(CharSequence path) {
-        BufferedReader bufferedReader = null;
-        try {
-            bufferedReader = new BufferedReader(new FileReader("/proc/mounts"), 256);
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                if (line.contains(path)) {
+    public static String[] getMounts(final String path)
+    {
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader("/proc/mounts"), 256);
+            String line = null;
+            while ((line = br.readLine()) != null)
+            {
+                if (line.contains(path))
+                {
                     return line.split(" ");
                 }
             }
-        } catch (FileNotFoundException ignored) {
+            br.close();
+        }
+        catch (FileNotFoundException e) {
             Log.d(TAG, "/proc/mounts does not exist");
-        } catch (IOException ignored) {
+        }
+        catch (IOException e) {
             Log.d(TAG, "Error reading /proc/mounts");
-        } finally {
-            if (bufferedReader != null) {
-                try {
-                    bufferedReader.close();
-                } catch (IOException ignored) {
-                    // ignored
-                }
-            }
         }
         return null;
     }
 
-    public static boolean getMount(String mount) {
-        String[] mounts = getMounts("/system");
-        if (mounts != null && mounts.length >= 3) {
-            String device = mounts[0];
-            String path = mounts[1];
-            String point = mounts[2];
-            String preferredMountCmd = new String("mount -o " + mount + ",remount -t " + point + ' ' + device + ' ' + path);
-            if (CMDProcessor.runSuCommand(preferredMountCmd).success()) {
+    public static boolean getMount(final String mount)
+    {
+        final CMDProcessor cmd = new CMDProcessor();
+        final String mounts[] = getMounts("/system");
+        if (mounts != null
+                && mounts.length >= 3)
+        {
+            final String device = mounts[0];
+            final String path = mounts[1];
+            final String point = mounts[2];
+            if (cmd.su.runWaitFor("mount -o " + mount + ",remount -t " + point + " " + device + " " + path).success())
+            {
                 return true;
             }
         }
-        String fallbackMountCmd = new String("busybox mount -o remount," + mount + " /system");
-        return CMDProcessor.runSuCommand(fallbackMountCmd).success();
+        return ( cmd.su.runWaitFor("busybox mount -o remount," + mount + " /system").success() );
     }
 
-    public static String readOneLine(String fname) {
-        BufferedReader br = null;
-        String line = null;
-        try {
-            br = new BufferedReader(new FileReader(fname), 1024);
-            line = br.readLine();
-        } catch (FileNotFoundException ignored) {
-            Log.d(TAG, "File was not found! trying via shell...");
-            return readFileViaShell(fname, true);
-        } catch (IOException e) {
-            Log.d(TAG, "IOException while reading system file", e);
-            return readFileViaShell(fname, true);
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException ignored) {
-                    // failed to close reader
+    public static String getFile(final String filename) {
+        String s = "";
+        final File f = new File(filename);
+
+        if (f.exists() && f.canRead()) {
+            try {
+                final BufferedReader br = new BufferedReader(new FileReader(f),
+                        256);
+                String buffer = null;
+                while ((buffer = br.readLine()) != null) {
+                    s += buffer + "\n";
                 }
+
+                br.close();
+            } catch (final Exception e) {
+                Log.e(TAG, "Error reading file: " + filename, e);
+                s = null;
             }
         }
-        return line;
+        return s;
     }
 
-    public static String readFileViaShell(String filePath, boolean useSu) {
-        String command = new String("cat " + filePath);
-        return useSu ? CMDProcessor.runSuCommand(command).getStdout()
-                : CMDProcessor.runShellCommand(command).getStdout();
-    }
-
-    public static boolean writeOneLine(String filename, String value) {
-        FileWriter fileWriter = null;
-        try {
-            fileWriter = new FileWriter(filename);
-            fileWriter.write(value);
-        } catch (IOException e) {
-            String Error = "Error writing { " + value + " } to file: " + filename;
-            Log.e(TAG, Error, e);
-            return false;
-        } finally {
-            if (fileWriter != null) {
-                try {
-                    fileWriter.close();
-                } catch (IOException ignored) {
-                    // failed to close writer
-                }
-            }
+    public static void writeNewFile(String filePath, String fileContents) {
+        File f = new File(filePath);
+        if (f.exists()) {
+            f.delete();
         }
-        return true;
-    }
 
-    public static String[] getAvailableIOSchedulers() {
-        String[] schedulers = null;
-        String[] aux = readStringArray("/sys/block/mmcblk0/queue/scheduler");
-        if (aux != null) {
-            schedulers = new String[aux.length];
-            for (int i = 0; i < aux.length; i++) {
-                schedulers[i] = aux[i].charAt(0) == '['
-                        ? aux[i].substring(1, aux[i].length() - 1)
-                        : aux[i];
-            }
+        try{
+            // Create file
+            FileWriter fstream = new FileWriter(f);
+            BufferedWriter out = new BufferedWriter(fstream);
+            out.write(fileContents);
+            //Close the output stream
+            out.close();
+        }catch (Exception e){
+            Log.d( TAG, "Failed to create " + filePath + " File contents: " + fileContents);
         }
-        return schedulers;
-    }
-
-    private static String[] readStringArray(String fname) {
-        String line = readOneLine(fname);
-        if (line != null) {
-            return line.split(" ");
-        }
-        return null;
-    }
-
-    public static String getIOScheduler() {
-        String scheduler = null;
-        String[] schedulers = readStringArray("/sys/block/mmcblk0/queue/scheduler");
-        if (schedulers != null) {
-            for (String s : schedulers) {
-                if (s.charAt(0) == '[') {
-                    scheduler = s.substring(1, s.length() - 1);
-                    break;
-                }
-            }
-        }
-        return scheduler;
     }
 
     /**
      * Long toast message
      *
-     * @param context Application Context
+     * @param c Application Context
      * @param msg Message to send
      */
-    public static void msgLong(Context context, String msg) {
-        if (context != null && msg != null) {
-            Toast.makeText(context, msg.trim(), Toast.LENGTH_LONG).show();
+    public static void msgLong(final Context c, final String msg) {
+        if (c != null && msg != null) {
+            Toast.makeText(c, msg.trim(), Toast.LENGTH_LONG).show();
         }
     }
 
     /**
      * Short toast message
      *
-     * @param context Application Context
+     * @param c Application Context
      * @param msg Message to send
      */
-    public static void msgShort(Context context, String msg) {
-        if (context != null && msg != null) {
-            Toast.makeText(context, msg.trim(), Toast.LENGTH_SHORT).show();
+    public static void msgShort(final Context c, final String msg) {
+        if (c != null && msg != null) {
+            Toast.makeText(c, msg.trim(), Toast.LENGTH_SHORT).show();
         }
     }
 
     /**
      * Long toast message
      *
-     * @param context Application Context
+     * @param c Application Context
      * @param msg Message to send
      */
-    public static void sendMsg(Context context, String msg) {
-        if (context != null && msg != null) {
-            msgLong(context, msg);
+    public static void sendMsg(final Context c, final String msg) {
+        if (c != null && msg != null) {
+            msgLong(c, msg);
         }
     }
 
-    /**
-     * Return a timestamp
-     *
-     * @param context Application Context
-     */
-    @SuppressWarnings("UnnecessaryFullyQualifiedName")
-    public static String getTimestamp(Context context) {
-        String timestamp = "unknown";
-        Date now = new Date();
-        java.text.DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(context);
-        java.text.DateFormat timeFormat = android.text.format.DateFormat.getTimeFormat(context);
-        if (dateFormat != null && timeFormat != null) {
-            timestamp = dateFormat.format(now) + ' ' + timeFormat.format(now);
-        }
-        return timestamp;
-    }
-
-    public static boolean isPackageInstalled(String packageName, PackageManager pm) {
+    public static boolean isPackageInstalled(final String packageName,
+            final PackageManager pm)
+    {
+        String mVersion;
         try {
-            String mVersion = pm.getPackageInfo(packageName, 0).versionName;
-            if (mVersion == null) {
+            mVersion = pm.getPackageInfo(packageName, 0).versionName;
+            if (mVersion.equals(null)) {
                 return false;
             }
-        } catch (NameNotFoundException notFound) {
-            Log.e(TAG, "Package could not be found!", notFound);
+        } catch (NameNotFoundException e) {
             return false;
         }
         return true;
     }
 
     public static void restartSystemUI() {
-        CMDProcessor.startSuCommand("pkill -TERM -f com.android.systemui");
+        new CMDProcessor().su.run("pkill -TERM -f com.android.systemui");
     }
 
-    public static void setSystemProp(String prop, String val) {
-        CMDProcessor.startSuCommand("setprop " + prop + " " + val);
-    }
-
-    public static String getSystemProp(String prop, String def) {
-        String result = null;
+    /*
+     * Find value of build.prop item (/system can be ro or rw)
+     *
+     * @param prop /system/build.prop property name to find value of
+     *
+     * @returns String value of @param:prop
+     */
+    public static String findBuildPropValueOf(String prop) {
+        String mBuildPath = "/system/build.prop";
+        String DISABLE = "disable";
+        String value = null;
         try {
-            result = SystemProperties.get(prop, def);
-        } catch (IllegalArgumentException iae) {
-            Log.e(TAG, "Failed to get prop: " + prop);
+            //create properties construct and load build.prop
+            Properties mProps = new Properties();
+            mProps.load(new FileInputStream(mBuildPath));
+            //get the property
+            value = mProps.getProperty(prop, DISABLE);
+            Log.d(TAG, String.format("Helpers:findBuildPropValueOf found {%s} with the value (%s)", prop, value));
+        } catch (IOException ioe) {
+            Log.d(TAG, "failed to load input stream");
+        } catch (NullPointerException npe) {
+            //swallowed thrown by ill formatted requests
         }
-        return result == null ? def : result;
+
+        if (value != null) {
+            return value;
+        } else {
+            return DISABLE;
+        }
     }
 }
