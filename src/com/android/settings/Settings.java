558ee1c00b1142a6df1c0531c7f0e373c6a61fb5
Alex Cruz <mazdarider23@gmail.com>
2014-02-01 22:07:01 -0600

    Fix derp with switches
    There was an issue where BT was sometimes turning on or at least, appearing that it was on if you turned on location or TRDS switch.
    
    Change-Id: I9985952468272a46de513e01e3a0c04c3d029781

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -964,11 +964,12 @@ public class Settings extends PreferenceActivity
                     // Would need a different treatment if the main menu had more switches
                     if (header.id == R.id.wifi_settings) {
                         mWifiEnabler.setSwitch(holder.switch_);
-                    } else if (header.id == R.id.location_settings) {
-                        mLocationEnabler.setSwitch(holder.switch_);
-                        mBluetoothEnabler.setSwitch(holder.switch_);
                     } else if (header.id == R.id.theme_settings) {
                         mThemeEnabler.setSwitch(holder.switch_);
+                    } else if (header.id == R.id.location_settings) {
+                        mLocationEnabler.setSwitch(holder.switch_);
+                    } else {
+                        mBluetoothEnabler.setSwitch(holder.switch_);
                     }
                     updateCommonHeaderView(header, holder);
                     break;
