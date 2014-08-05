02b33d1769e6f68cfd760658d460f611d149f48d
Alex Cruz <mazdarider23@gmail.com>
2014-08-05 10:31:40 -0400

    MIUI Style carrier text in the statusbar [2/2]
    Change-Id: Ic053615e86401972a15053142191d561fd82e89b

--- src/com/android/settings/du/CarrierLabel.java
+++ src/com/android/settings/du/CarrierLabel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Gummy
+ * Copyright (C) 2014 The Dirty Unicorns Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ package com.android.settings.du;
 import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.content.ContentResolver;
+import android.database.ContentObserver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -32,11 +33,14 @@ import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 import android.text.Spannable;
+import android.text.TextUtils;
 import android.widget.EditText;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.utils.Helpers;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 public class CarrierLabel extends SettingsPreferenceFragment  implements
         Preference.OnPreferenceChangeListener {
@@ -46,11 +50,17 @@ public class CarrierLabel extends SettingsPreferenceFragment  implements
     private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
     private static final String PREF_NOTIFICATION_SHOW_WIFI_SSID = "notification_show_wifi_ssid";
     private static final String NOTIFICATION_SHORTCUTS_HIDE_CARRIER = "pref_notification_shortcuts_hide_carrier";
+    private static final String STATUS_BAR_CARRIER = "status_bar_carrier";
+    private static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
+
+    static final int DEFAULT_STATUS_CARRIER_COLOR = 0xffffffff;
 
     private ContentResolver mCr;
     private PreferenceScreen mPrefSet;
 
     private CheckBoxPreference mHideCarrier;
+    private CheckBoxPreference mStatusBarCarrier;
+    private ColorPickerPreference mCarrierColorPicker;
 
     Preference mCustomLabel;
     String mCustomLabelText = null;
@@ -61,7 +71,7 @@ public class CarrierLabel extends SettingsPreferenceFragment  implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.carrier_label_prefs);
+        addPreferencesFromResource(R.xml.carrierlabel);
         PreferenceScreen prefScreen = getPreferenceScreen();
 
         ActionBar actionBar = getActivity().getActionBar();
@@ -70,13 +80,31 @@ public class CarrierLabel extends SettingsPreferenceFragment  implements
         mCr = getContentResolver();
         mPrefSet = getPreferenceScreen();
 
+        int intColor;
+        String hexColor;
+
         // Custom Carrier Label Text
         mCustomLabel = findPreference(PREF_CUSTOM_CARRIER_LABEL);
         updateCustomLabelTextSummary();
 
+
         // Show Wifi Network Name
         mShowWifiName = (CheckBoxPreference) findPreference(PREF_NOTIFICATION_SHOW_WIFI_SSID);
             mShowWifiName.setOnPreferenceChangeListener(this);
+
+        // MIUI-like carrier Label
+        mStatusBarCarrier = (CheckBoxPreference) findPreference(STATUS_BAR_CARRIER);
+        mStatusBarCarrier.setChecked((Settings.System.getInt(mCr,
+                Settings.System.STATUS_BAR_CARRIER, 0) == 1));
+
+        // MIUI-like carrier Label color
+        mCarrierColorPicker = (ColorPickerPreference) mPrefSet.findPreference(STATUS_BAR_CARRIER_COLOR);
+        mCarrierColorPicker.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_CARRIER_COLOR, DEFAULT_STATUS_CARRIER_COLOR);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mCarrierColorPicker.setSummary(hexColor);
+        mCarrierColorPicker.setNewPreviewColor(intColor);
 
         // Hide Carrier Label
         mHideCarrier = (CheckBoxPreference) mPrefSet.findPreference(
@@ -88,7 +116,6 @@ public class CarrierLabel extends SettingsPreferenceFragment  implements
             public boolean onPreferenceChange(Preference preference,
                         Object newValue) {
                 Settings.System.putInt(mCr, Settings.System.NOTIFICATION_SHORTCUTS_HIDE_CARRIER, (Boolean) newValue ? 1 : 0);
-                Helpers.restartSystemUI();
                 return true;
             }
         });
@@ -106,7 +133,10 @@ public class CarrierLabel extends SettingsPreferenceFragment  implements
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mCustomLabel) {
+        if (preference == mStatusBarCarrier) {
+            Settings.System.putInt(mCr, Settings.System.STATUS_BAR_CARRIER, mStatusBarCarrier.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mCustomLabel) {
             AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
 
             alert.setTitle(R.string.custom_carrier_label_title);
@@ -126,6 +156,7 @@ public class CarrierLabel extends SettingsPreferenceFragment  implements
                     Intent i = new Intent();
                     i.setAction("com.android.settins.LABEL_CHANGED");
                     getActivity().sendBroadcast(i);
+                    Helpers.restartSystemUI();
                 }
             });
             alert.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
@@ -139,8 +170,16 @@ public class CarrierLabel extends SettingsPreferenceFragment  implements
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-         if (preference == mShowWifiName) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mCarrierColorPicker) {
+            String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.STATUS_BAR_CARRIER_COLOR, intHex);
+            return true;
+        } else if (preference == mShowWifiName) {
             Settings.System.putInt(getActivity().getContentResolver(), Settings.System.NOTIFICATION_SHOW_WIFI_SSID,
                     ((CheckBoxPreference)preference).isChecked() ? 0 : 1);
             return true;
