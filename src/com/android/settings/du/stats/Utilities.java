89ca9aa0286036774cc2b9bd1eb8425835d28b60
Alex Cruz <mazdarider23@gmail.com>
2014-10-15 14:54:58 -0400

    Last kitkat release before L [2/2]
    Change-Id: I4dac27700fefb2ecd02f4057d15e49212faa4446

--- src/com/android/settings/du/stats/Utilities.java
+++ src/com/android/settings/du/stats/Utilities.java
@@ -106,7 +106,7 @@ public class Utilities {
 	}
 
 	public static String getRomVersion() {
-		return SystemProperties.get("ro.romstats.version", "8.0");
+		return SystemProperties.get("ro.romstats.version", "8.1");
 	}
 
 	public static long getTimeFrame() {
