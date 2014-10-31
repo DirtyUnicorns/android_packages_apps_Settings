68c9ad5c0e83279d9a4a0d6475d6b8d58e40cf8b
Alex Cruz <mazdarider23@gmail.com>
2014-10-30 22:52:12 -0400

    Move Blacklist and Privacy Guard up
    I'm getting tired of being asked if we have this and is because most can't find it
    
    Change-Id: I498ab79203b0510c49741f552228897164967e6d

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -198,6 +198,18 @@ public class SecuritySettings extends RestrictedSettingsFragment
         // Add package manager to check if features are available
         PackageManager pm = getPackageManager();
 
+        // App security settings
+        addPreferencesFromResource(R.xml.security_settings_app_custom);
+        mBlacklist = (PreferenceScreen) root.findPreference(KEY_BLACKLIST);
+
+        // Determine options based on device telephony support
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+            // No telephony, remove dependent options
+            PreferenceGroup appCategory = (PreferenceGroup)
+                    root.findPreference(KEY_APP_SECURITY_CATEGORY);
+            appCategory.removePreference(mBlacklist);
+        }
+
         // Add options for lock/unlock screen
         int resid = 0;
         if (!mLockPatternUtils.isSecure()) {
@@ -425,18 +437,6 @@ public class SecuritySettings extends RestrictedSettingsFragment
             } else {
                 mToggleVerifyApps.setEnabled(false);
             }
-        }
-
-        // App security settings
-        addPreferencesFromResource(R.xml.security_settings_app_custom);
-        mBlacklist = (PreferenceScreen) root.findPreference(KEY_BLACKLIST);
-
-        // Determine options based on device telephony support
-        if (!pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
-            // No telephony, remove dependent options
-            PreferenceGroup appCategory = (PreferenceGroup)
-                    root.findPreference(KEY_APP_SECURITY_CATEGORY);
-            appCategory.removePreference(mBlacklist);
         }
 
         mNotificationAccess = findPreference(KEY_NOTIFICATION_ACCESS);
