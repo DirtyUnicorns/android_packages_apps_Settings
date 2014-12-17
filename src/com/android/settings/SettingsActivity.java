b043be563ca9e314b34e2827ae1b39548d2ddc35
Mazda <mazdarider23@gmail.com>
2014-12-16 22:19:30 -0500

    Add customization section / Dirty Tweaks
    Change-Id: I326f505d5d489ba4fe3ea1d7d5e5f9960bd5dcb1

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1196,6 +1196,16 @@ public class SettingsActivity extends Activity
                             UserManager.DISALLOW_DEBUGGING_FEATURES)) {
                         removeTile = true;
                     }
+                } else if (id == R.id.dirtytweaks) {
+                    // Not sure why any user would uninstall Dirty Tweaks but if they do, let's remove this
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("com.dirtyunicorns.dutweaks", 0).versionCode >= 1);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
                 } else if (id == R.id.supersu_settings) {
                     // Embedding into Settings is supported from SuperSU v1.85 and up
                     boolean supported = false;
