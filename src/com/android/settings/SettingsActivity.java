35a92895f20b1cce5ab70c5f551b055007905977
Alex Cruz <mazdarider23@gmail.com>
2015-02-20 06:09:34 +0000

    NavBar tile [2/3]
    Change-Id: I684e370b813d03782d01e2abc0d28f06560a4cf0

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -114,6 +114,7 @@ import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.dirtyunicorns.dutweaks.fragments.HeadsUpSettings;
+import com.dirtyunicorns.dutweaks.fragments.NavBar;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -302,7 +303,8 @@ public class SettingsActivity extends Activity
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
             DirtyTweaks.class.getName(),
-            HeadsUpSettings.class.getName()
+            HeadsUpSettings.class.getName(),
+            NavBar.class.getName()
     };
 
 
