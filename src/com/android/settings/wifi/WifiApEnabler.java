31e6e8feac943ece007b05b852501e5b001270c8
kaiyiz <kaiyiz@codeaurora.org>
2015-07-10 02:48:02 +0000

    Hotspot idle support
    - Added more values (30 and 60 minutes)
    
    Change-Id: Ic319087badda930942c8c392ab5be0d19b360e81

--- src/com/android/settings/wifi/WifiApEnabler.java
+++ src/com/android/settings/wifi/WifiApEnabler.java
@@ -17,10 +17,13 @@
 package com.android.settings.wifi;
 
 import com.android.settings.R;
+import com.android.settings.wifi.HotspotService;
 import com.android.settings.WirelessSettings;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -183,6 +186,7 @@ public class WifiApEnabler {
     }
 
     private void handleWifiApStateChanged(int state) {
+        String hotspotServiceClassName = HotspotService.class.getName();
         switch (state) {
             case WifiManager.WIFI_AP_STATE_ENABLING:
                 mSwitch.setSummary(R.string.wifi_tether_starting);
@@ -193,6 +197,12 @@ public class WifiApEnabler {
                  * Summary on enable is handled by tether
                  * broadcast notice
                  */
+                if (isWifiHotspotEnabled() &&
+                        (!isServiceRunning(mContext, hotspotServiceClassName))) {
+                    Intent intent = new Intent().setClassName(mContext,
+                            hotspotServiceClassName);
+                    mContext.startService(intent);
+                }
                 mSwitch.setChecked(true);
                 /* Doesnt need the airplane check */
                 mSwitch.setEnabled(true);
@@ -203,6 +213,12 @@ public class WifiApEnabler {
                 mSwitch.setEnabled(false);
                 break;
             case WifiManager.WIFI_AP_STATE_DISABLED:
+                if (isWifiHotspotEnabled() &&
+                        isServiceRunning(mContext, hotspotServiceClassName)) {
+                    Intent intent = new Intent().setClassName(mContext,
+                            hotspotServiceClassName);
+                    mContext.stopService(intent);
+                }
                 mSwitch.setChecked(false);
                 mSwitch.setSummary(mOriginalSummary);
                 enableWifiSwitch();
@@ -212,5 +228,32 @@ public class WifiApEnabler {
                 mSwitch.setSummary(R.string.wifi_error);
                 enableWifiSwitch();
         }
+    }
+
+    private boolean isWifiHotspotEnabled() {
+        boolean isWifiHotspotEnable = false;
+        if (mContext != null) {
+            isWifiHotspotEnable = true;
+        }
+        return isWifiHotspotEnable;
+    }
+
+    private boolean isServiceRunning(Context context, String className) {
+        boolean isRunning = false;
+        if (context != null && (!TextUtils.isEmpty(className))) {
+            ActivityManager activityManager = (ActivityManager)
+                    context.getSystemService(Context.ACTIVITY_SERVICE);
+            List<ActivityManager.RunningServiceInfo> serviceList = activityManager
+                    .getRunningServices(100);
+            if (serviceList != null) {
+                for (int i = 0; i < serviceList.size(); i++) {
+                    if (serviceList.get(i).service.getClassName().equals(className)) {
+                        isRunning = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return isRunning;
     }
 }
