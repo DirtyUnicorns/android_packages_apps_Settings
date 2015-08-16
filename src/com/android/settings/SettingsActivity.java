14ebf73d5f19878ceab25c8d649f61e3ec20a7c7
Alex Cruz <mazdarider23@gmail.com>
2015-08-16 11:16:00 +0000

    Move slim device gestures out of display
    Change-Id: I015c8d36aa38357ecccae79ce3375244bb0ddd04

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1246,6 +1246,15 @@ public class SettingsActivity extends Activity
                     if (!supported) {
                         removeTile = true;
                     }
+                } else if (id == R.id.screen_off_gesture_settings) {
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("com.slim.device", 0).versionCode >= 1);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
                 }
 
                 if (UserHandle.MU_ENABLED && UserHandle.myUserId() != 0
