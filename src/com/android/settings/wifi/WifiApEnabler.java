95614ac106f6e47a66c3a65ef3126c2f8f64c5f6
Om Prakash Singh <omp@nvidia.com>
2015-10-14 15:42:10 +0530

    Fix synchronization of Wi-Fi hotspot switch
    Issue:
    with WiFi enabled when we toggle WiFi hotspot it also toggles WiFi state.
    in fast hotspot toggle, track of WiFi state is missing and on hotspot
    enable, WiFi is not getting disable hence CMD_SET_AP command is not being
    processed.
    
    Solution:
    If saved WiFi state is enabled, WiFi will restore to enabled state
    on softAP disable. In this case call enableWifiSwitch on WIFI_STATE_ENABLED
    event.
    
    Change-Id: Id70b93eafdc5bcb9afe7dca553906bafdf8e8545

--- src/com/android/settings/wifi/WifiApEnabler.java
+++ src/com/android/settings/wifi/WifiApEnabler.java
@@ -40,6 +40,7 @@ public class WifiApEnabler {
 
     private WifiManager mWifiManager;
     private final IntentFilter mIntentFilter;
+    private int mWifiSavedState = 0;
 
     ConnectivityManager mCm;
     private String[] mWifiRegexs;
@@ -68,6 +69,14 @@ public class WifiApEnabler {
                 updateTetherState(available.toArray(), active.toArray(), errored.toArray());
             } else if (Intent.ACTION_AIRPLANE_MODE_CHANGED.equals(action)) {
                 enableWifiSwitch();
+            } else if (WifiManager.WIFI_STATE_CHANGED_ACTION.equals(action)) {
+                if (intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,
+                WifiManager.WIFI_STATE_UNKNOWN) == WifiManager.WIFI_STATE_ENABLED) {
+                    if (mWifiSavedState == 1) {
+                        enableWifiSwitch();
+                        mWifiSavedState = 0;
+                    }
+                }
             }
         }
     };
@@ -88,6 +97,7 @@ public class WifiApEnabler {
         mIntentFilter = new IntentFilter(WifiManager.WIFI_AP_STATE_CHANGED_ACTION);
         mIntentFilter.addAction(ConnectivityManager.ACTION_TETHER_STATE_CHANGED);
         mIntentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        mIntentFilter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
     }
 
     public void resume() {
@@ -157,6 +167,8 @@ public class WifiApEnabler {
                 mSwitch.setChecked(true);
                 /* Doesnt need the airplane check */
                 mSwitch.setEnabled(!mDataSaverBackend.isDataSaverEnabled());
+                mWifiSavedState = Settings.Global.getInt(mContext.getContentResolver(),
+                                      Settings.Global.WIFI_SAVED_STATE, 0);
                 break;
             case WifiManager.WIFI_AP_STATE_DISABLING:
                 mSwitch.setSummary(R.string.wifi_tether_stopping);
@@ -166,7 +178,11 @@ public class WifiApEnabler {
             case WifiManager.WIFI_AP_STATE_DISABLED:
                 mSwitch.setChecked(false);
                 mSwitch.setSummary(mOriginalSummary);
-                enableWifiSwitch();
+                /* If saved WiFi state is enabled, WiFi will restore to enabled state
+                   on softAP disable. in this case call enableWifiSwitch on WIFI_STATE_ENABLED
+                   event */
+                if (mWifiSavedState == 0)
+                    enableWifiSwitch();
                 break;
             default:
                 mSwitch.setChecked(false);
