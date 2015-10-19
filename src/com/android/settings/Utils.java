a2da9c4ad84fa89df61fe49adcfa8fa1d3a43da1
Roman Birg <roman@cyngn.com>
2015-10-19 23:29:47 +0000

    Add isPackageInstalled boolean
    Change-Id: I9c4c6e876daf92442d9c85750f2ce3b03dd8bb0b

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1245,5 +1245,24 @@ public final class Utils {
             return UserHandle.myUserId();
         }
     }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
+    }
 }
 
