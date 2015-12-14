faedce62ea119295ca4026ed5771cda741c9d4d8
Alex Cruz <mazdarider23@gmail.com>
2015-12-14 15:45:51 +0000

    Fix longpress DU activity
    Change-Id: Ibd3861d5566083175fff8c1ee0269392869db147

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -118,6 +118,6 @@ public class Settings extends SettingsActivity {
     public static class WriteSettingsActivity extends SettingsActivity { /* empty */ }
     public static class AppDrawOverlaySettingsActivity extends SettingsActivity { /* empty */ }
     public static class AppWriteSettingsActivity extends SettingsActivity { /* empty */ }
-    public static class DuSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class DirtyTweaksActivity extends SettingsActivity { /* empty */ }
     public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
 }
