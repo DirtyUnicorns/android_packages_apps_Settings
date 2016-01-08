175b2d5cbcd92005a0bd7c2eb2ab46534c690695
Tomohiro Takeda <tomohiro.takeda@sonymobile.com>
2016-01-08 11:38:41 +0900

    Fix ApplicationsState.Session memory leak in settings app
    Removes the creation of an unused session and some other redundant
    code left behind when c5291b87 was applied.
    
    Change-Id: Ie5ec0f00b3240df9cf2c4fa3bd05b49ba45f4547

--- src/com/android/settings/applications/AdvancedAppSettings.java
+++ src/com/android/settings/applications/AdvancedAppSettings.java
@@ -29,14 +29,10 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.applications.PermissionsSummaryHelper.PermissionsResultCallback;
 import com.android.settings.fuelgauge.PowerWhitelistBackend;
-import com.android.settingslib.applications.ApplicationsState;
-import com.android.settingslib.applications.ApplicationsState.AppEntry;
-import com.android.settingslib.applications.ApplicationsState.Session;
 
 import java.util.ArrayList;
 
-public class AdvancedAppSettings extends SettingsPreferenceFragment implements
-        ApplicationsState.Callbacks {
+public class AdvancedAppSettings extends SettingsPreferenceFragment {
 
     static final String TAG = "AdvancedAppSettings";
 
@@ -46,7 +42,6 @@ public class AdvancedAppSettings extends SettingsPreferenceFragment implements
     private static final String KEY_SYSTEM_ALERT_WINDOW = "system_alert_window";
     private static final String KEY_WRITE_SETTINGS_APPS = "write_settings_apps";
 
-    private Session mSession;
     private Preference mAppPermsPreference;
     private Preference mAppDomainURLsPreference;
     private Preference mHighPowerPreference;
@@ -63,10 +58,6 @@ public class AdvancedAppSettings extends SettingsPreferenceFragment implements
         Preference permissions = getPreferenceScreen().findPreference(KEY_APP_PERM);
         permissions.setIntent(new Intent(Intent.ACTION_MANAGE_PERMISSIONS));
 
-        ApplicationsState applicationsState = ApplicationsState.getInstance(
-                getActivity().getApplication());
-        mSession = applicationsState.newSession(this);
-
         mAppPermsPreference = findPreference(KEY_APP_PERM);
         mAppDomainURLsPreference = findPreference(KEY_APP_DOMAIN_URLS);
         mHighPowerPreference = findPreference(KEY_HIGH_POWER_APPS);
@@ -77,46 +68,6 @@ public class AdvancedAppSettings extends SettingsPreferenceFragment implements
     @Override
     protected int getMetricsCategory() {
         return MetricsLogger.APPLICATIONS_ADVANCED;
-    }
-
-    @Override
-    public void onRunningStateChanged(boolean running) {
-        // No-op.
-    }
-
-    @Override
-    public void onPackageListChanged() {
-        // No-op.
-    }
-
-    @Override
-    public void onRebuildComplete(ArrayList<AppEntry> apps) {
-        // No-op.
-    }
-
-    @Override
-    public void onPackageIconChanged() {
-        // No-op.
-    }
-
-    @Override
-    public void onPackageSizeChanged(String packageName) {
-        // No-op.
-    }
-
-    @Override
-    public void onAllSizesComputed() {
-        // No-op.
-    }
-
-    @Override
-    public void onLauncherInfoChanged() {
-        // No-op.
-    }
-
-    @Override
-    public void onLoadEntriesCompleted() {
-        // No-op.
     }
 
     private final PermissionsResultCallback mPermissionCallback = new PermissionsResultCallback() {
