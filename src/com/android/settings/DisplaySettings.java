8bfc3003f6ab45f28b0df96546dada0c88c19331
Alex Cruz <mazdarider23@gmail.com>
2016-01-21 11:06:02 -0500

    Configurable dashboard columns [1/2]
    Before this, the only way to get double columns on a device like the Nexus 6
    was to adjust the config.xml and while that works for some, it doesn't for all.
    
    This method allows those that like to adjust the columns to do so while keeping
    the stock look :-)
    
    Thanks to @xWASABI for the idea in where he adjusted the config.xml
    https://github.com/TeamTwisted/packages_apps_Settings/commit/3c62ed2f244ae21436e02556894c2f5b46365024
    
    Change-Id: Id99ad00974ec5fe3aeef8e9cbabf6fe156c54bf2

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -61,6 +61,7 @@ import android.util.Log;
 
 import com.android.settings.du.DisplayRotation;
 import com.android.settings.du.NumberPickerPreference;
+import com.android.settings.dashboard.DashboardContainerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -87,6 +88,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_DISPLAY_ROTATION = "display_rotation";
     private static final String DT2L_TARGET_VIBRATE_CONFIG = "dt2l_target_vibrate_config";
 
+    private static final String DASHBOARD_COLUMNS = "dashboard_columns";
+
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
     private WarnedListPreference mFontSizePref;
@@ -105,6 +108,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private SwitchPreference mCameraDoubleTapPowerGesturePreference;
     private SwitchPreference mWakeWhenPluggedOrUnplugged;
     private NumberPickerPreference mDt2lTargetVibrateConfig;
+    private ListPreference mDashboardColumns;
 
     private ContentResolver mCr;
     private PreferenceScreen mPrefSet;
@@ -168,6 +172,12 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         mDt2lTargetVibrateConfig.setMaxValue(MAX_VIB_VALUE);
         int cvConfig = Settings.System.getInt(mCr, Settings.System.DT2L_TARGET_VIBRATE_CONFIG, 1);
         mDt2lTargetVibrateConfig.setCurrentValue(cvConfig);
+
+        mDashboardColumns = (ListPreference) findPreference(DASHBOARD_COLUMNS);
+        mDashboardColumns.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.DASHBOARD_COLUMNS, DashboardContainerView.mDashboardValue)));
+        mDashboardColumns.setSummary(mDashboardColumns.getEntry());
+        mDashboardColumns.setOnPreferenceChangeListener(this);
 
         if (isAutomaticBrightnessAvailable(getResources())) {
             mAutoBrightnessPreference = (SwitchPreference) findPreference(KEY_AUTO_BRIGHTNESS);
@@ -562,6 +572,13 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             int value = Integer.parseInt(objValue.toString());
             Settings.System.putInt(mCr, Settings.System.DT2L_TARGET_VIBRATE_CONFIG,
                     value);
+        }
+        if (preference == mDashboardColumns) {
+            Settings.System.putInt(getContentResolver(), Settings.System.DASHBOARD_COLUMNS,
+                    Integer.valueOf((String) objValue));
+            mDashboardColumns.setValue(String.valueOf(objValue));
+            mDashboardColumns.setSummary(mDashboardColumns.getEntry());
+            return true;
         }
         if (preference == mCameraDoubleTapPowerGesturePreference) {
             boolean value = (Boolean) objValue;
