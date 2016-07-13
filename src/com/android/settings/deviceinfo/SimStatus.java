5b22f6f515fc458bc7935166715777506dd6fef9
nickdoh81 <nickdoh81@gmail.com>
2016-07-13 02:43:13 -0400

    Revert "Use same technology type for LTE/4G"
    This reverts commit 7995ca4165cae43b9bff25a6e6eafdb51445b698.
    
    Change-Id: Iec6aa03b930e9627fafcdea5102568d5101148db

--- src/com/android/settings/deviceinfo/SimStatus.java
+++ src/com/android/settings/deviceinfo/SimStatus.java
@@ -22,7 +22,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -257,19 +256,6 @@ public class SimStatus extends InstrumentedPreferenceActivity {
             networktype = mTelephonyManager.getNetworkTypeName(actualVoiceNetworkType);
         }
 
-        boolean show4GForLTE = false;
-        try {
-            Context con = createPackageContext("com.android.systemui", 0);
-            int id = con.getResources().getIdentifier("config_show4GForLTE",
-                    "bool", "com.android.systemui");
-            show4GForLTE = con.getResources().getBoolean(id);
-        } catch (NameNotFoundException e) {
-            Log.e(TAG, "NameNotFoundException for show4GFotLTE");
-        }
-
-        if (networktype != null && networktype.equals("LTE") && show4GForLTE) {
-            networktype = "4G";
-        }
         setSummaryText(KEY_NETWORK_TYPE, networktype);
     }
 
