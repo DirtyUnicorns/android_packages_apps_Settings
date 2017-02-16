6fa446989a25e3855dd574be88614628d8f32057
Danny Baumann <dannybaumann@web.de>
2017-02-16 14:50:56 +0100

    Fix possible NPE on SIM settings shutdown.
    Change-Id: Ibf6b54fc2b97a780cfd7ace17c54855ef7e70c57

--- src/com/android/settings/sim/SimSettings.java
+++ src/com/android/settings/sim/SimSettings.java
@@ -106,7 +106,9 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
         @Override
         public void onSubscriptionsChanged() {
             if (DBG) log("onSubscriptionsChanged:");
-            updateSubscriptions();
+            if (isAdded()) {
+                updateSubscriptions();
+            }
         }
     };
 
