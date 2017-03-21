330b661a2bc8258bc5cc9948134634b4421cbe08
Huang, Yang <yang.huang@intel.com>
2017-03-21 13:56:40 +0800

    Redundant waiting time of keyguard unlocking after reboot
    LOCKOUT_ATTEMPT_DEADLINE and LOCKOUT_ATTEMPT_TIMEOUT_MS
    should be cleared for every successful screen unlock.
    
    Change-Id: Ibc4d2e73d14405c498bf23312837d53fa791dea9
    Signed-off-by: Huang, Yang <yang.huang@intel.com>

--- src/com/android/settings/ConfirmLockPattern.java
+++ src/com/android/settings/ConfirmLockPattern.java
@@ -495,6 +495,7 @@ public class ConfirmLockPattern extends ConfirmDeviceCredentialBaseActivity {
                 if (newResult) {
                     reportSuccessfullAttempt();
                 }
+                mLockPatternUtils.getLockoutAttemptDeadline(effectiveUserId);
                 startDisappearAnimation(intent);
                 checkForPendingIntent();
             } else {
