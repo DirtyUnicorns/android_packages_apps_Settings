40a3ec3c49535a73af65d8bfb53069116ef33ada
Chien-Yu Chen <cychen@google.com>
2017-11-10 13:51:24 -0800

    Settings: Remove HAL HDR+ option
    Test: RunSettingsRoboTests
    Bug: 68223991
    Merged-In: I9eb27c7e205ac005c6a71726e125ed1e512da33b
    Change-Id: I9eb27c7e205ac005c6a71726e125ed1e512da33b

--- src/com/android/settings/development/DevelopmentSettings.java
+++ src/com/android/settings/development/DevelopmentSettings.java
@@ -364,7 +364,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     //private BugReportPreferenceController mBugReportController;
     //private BugReportInPowerPreferenceController mBugReportInPowerController;
     private TelephonyMonitorPreferenceController mTelephonyMonitorController;
-    private CameraHalHdrplusPreferenceController mCameraHalHdrplusController;
     private CameraLaserSensorPreferenceController mCameraLaserSensorController;
 
     private BroadcastReceiver mEnableAdbReceiver;
@@ -409,7 +408,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         mTelephonyMonitorController = new TelephonyMonitorPreferenceController(getActivity());
         mWebViewAppPrefController = new WebViewAppPreferenceController(getActivity());
         mVerifyAppsOverUsbController = new VerifyAppsOverUsbPreferenceController(getActivity());
-        mCameraHalHdrplusController = new CameraHalHdrplusPreferenceController(getActivity());
         mCameraLaserSensorController = new CameraLaserSensorPreferenceController(getActivity());
 
         setIfOnlyAvailableForAdmins(true);
@@ -443,7 +441,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         //mBugReportInPowerController.displayPreference(getPreferenceScreen());
         mTelephonyMonitorController.displayPreference(getPreferenceScreen());
         mWebViewAppPrefController.displayPreference(getPreferenceScreen());
-        mCameraHalHdrplusController.displayPreference(getPreferenceScreen());
         mEnableAdbController.displayPreference(getPreferenceScreen());
 
         mCameraLaserSensorController.displayPreference(getPreferenceScreen());
@@ -684,7 +681,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         //mBugReportInPowerController.enablePreference(enabled);
         mTelephonyMonitorController.enablePreference(enabled);
         mWebViewAppPrefController.enablePreference(enabled);
-        mCameraHalHdrplusController.enablePreference(enabled);
         mCameraLaserSensorController.enablePreference(enabled);
         updateAllOptions();
     }
@@ -819,7 +815,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         }
         //mHaveDebugSettings |= mBugReportInPowerController.updatePreference();
         mHaveDebugSettings |= mTelephonyMonitorController.updatePreference();
-        mHaveDebugSettings |= mCameraHalHdrplusController.updatePreference();
         mHaveDebugSettings |= mCameraLaserSensorController.updatePreference();
         updateSwitchPreference(mKeepScreenOn, Settings.Global.getInt(cr,
                 Settings.Global.STAY_ON_WHILE_PLUGGED_IN, 0) != 0);
@@ -2487,10 +2482,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         }
 
         if (mVerifyAppsOverUsbController.handlePreferenceTreeClick(preference)) {
-            return true;
-        }
-
-        if (mCameraHalHdrplusController.handlePreferenceTreeClick(preference)) {
             return true;
         }
 
