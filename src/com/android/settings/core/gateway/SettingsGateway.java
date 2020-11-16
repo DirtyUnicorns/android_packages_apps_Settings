b30846f59b79147b650c7f767ecbec993cc7a2d9
maxwen <max.weninger@gmail.com>
2020-11-16 07:07:47 -0500

    Fully integrate DU-Tweaks into Settings
    * This helps us avoid any permissions type of issues that we had before and makes for a better experience for the user.
    
    Change-Id: I96bdde0616f369dce5b4447086d478b739ba125d

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -160,6 +160,8 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.dirtyunicorns.tweaks.DirtyTweaks;
+
 public class SettingsGateway {
 
     /**
@@ -307,7 +309,8 @@ public class SettingsGateway {
             GestureNavigationSettingsFragment.class.getName(),
             InteractAcrossProfilesSettings.class.getName(),
             InteractAcrossProfilesDetails.class.getName(),
-            MediaControlsSettings.class.getName()
+            MediaControlsSettings.class.getName(),
+            DirtyTweaks.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -347,5 +350,7 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            // Home page > Dirty Tweaks
+            Settings.DirtyTweaksActivity.class.getName()
     };
 }
